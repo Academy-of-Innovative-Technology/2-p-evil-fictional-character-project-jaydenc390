4a0873ba78d50c6d63722c640357145321e11f39
jaydenc390 <jaydenc390@nycstudents.net>
2025-11-20 13:10:21 -0500

    so lost

---
+++ script2.js
@@ -0,0 +1,34 @@
+function el(target, all) {
+    if (all) {
+        return document.querySelectorAll(target);
+    } else {
+        return document.querySelector(target);
+    }
+}
+
+const aboutPage2 = document.querySelector(".about-page2");
+const main = document.querySelector("main");
+
+el(".page1, .about-page", true).forEach(btn => {
+    main.addEventListener("click", function () {
+        if (main.style.display === "block") {
+            main.style.display = "none";
+            aboutPage2.style.display = "block";
+        } else if (main.style.display === "none") {
+            main.style.display = "block";
+        }
+    });
+});
+
+
+/* 
+el("h1").addEventListener("click", function() {
+  // Toggle display between "none" and "block"
+  const h1 = el("h1");
+  if (h1.style.display === "none") {
+    h1.style.display = "block"; // Make the element visible
+  } else {
+    h1.style.display = "none";  // Hide the element
+  }
+});
+*/
